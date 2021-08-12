ce5a22d6676b6d20be4a16f0d5e63f01a0f1c523
jowest90 <jowest1990@gmail.com>
2021-08-11 21:17:24 -0500

    Spring boot project
    added spring boot project

---
+++ src/main/java/org/launchcode/drinkapp/DrinkAppApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.drinkapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrinkAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrinkAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/drinkapp/DrinkAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.drinkapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrinkAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
