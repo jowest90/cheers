f17d0d30ec04d690e47a691e810fa1e372ec402b
jowest90 <jowest1990@gmail.com>
2021-09-22 09:37:35 -0500

    rating system almost there

--- src/main/java/org/launchcode/drinkapp/models/Rating.java
+++ src/main/java/org/launchcode/drinkapp/models/Rating.java
@@ -21,6 +21,31 @@ public class Rating {
     @Size(min = 1,max = 5, message = "rating must be between 1-5")
     private int rating;
 
+    public Rating() {
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Drink getDrink() {
+        return drink;
+    }
+
+    public void setDrink(Drink drink) {
+        this.drink = drink;
+    }
+
+    public Rating(User user, Drink drink) {
+        super();
+        this.user = user;
+        this.drink = drink;
+    }
+
     public int getId() {
         return id;
     }
